afb41b16cad54b13d2ebc7d4ac5a06a2b68827f5
drago <93286752+drago44@users.noreply.github.com>
2026-04-24 15:51:22 +0300

    feat: implement vault state initialization with PDA derived seeds and bump storage

--- 06-anchor-vault/programs/anchor_vault/src/instructions/initialize.rs
+++ 06-anchor-vault/programs/anchor_vault/src/instructions/initialize.rs
@@ -1,9 +1,32 @@
 use anchor_lang::prelude::*;
 
+use crate::state::VaultState;
+
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(mut)]
+    pub owner: Signer<'info>,
+
+    #[account(
+        init,
+        payer = owner,
+        space = VaultState::INIT_SPACE,
+        seeds = [b"state", owner.key().as_ref()],
+        bump
+    )]
+    pub vault_state: Account<'info, VaultState>,
+
+    #[account(
+        seeds = [b"vault", owner.key().as_ref()],
+        bump
+    )]
+    pub vault: SystemAccount<'info>,
+
+    pub system_program: Program<'info, System>,
+}
 
 pub fn handler(ctx: Context<Initialize>) -> Result<()> {
-    msg!("Greetings from: {:?}", ctx.program_id);
+    ctx.accounts.vault_state.vault_bump = ctx.bumps.vault;
+    ctx.accounts.vault_state.state_bump = ctx.bumps.vault_state;
     Ok(())
 }
--- 06-anchor-vault/programs/anchor_vault/tests/test_initialize.rs
+++ 06-anchor-vault/programs/anchor_vault/tests/test_initialize.rs
@@ -3,9 +3,18 @@ use {
     litesvm::LiteSVM,
     solana_keypair::Keypair,
     solana_message::{Message, VersionedMessage},
+    solana_pubkey::Pubkey,
     solana_signer::Signer,
     solana_transaction::versioned::VersionedTransaction,
 };
+
+fn get_vault_state_pda(owner: &Pubkey, program_id: &Pubkey) -> (Pubkey, u8) {
+    Pubkey::find_program_address(&[b"state", owner.as_ref()], program_id)
+}
+
+fn get_vault_pda(owner: &Pubkey, program_id: &Pubkey) -> (Pubkey, u8) {
+    Pubkey::find_program_address(&[b"vault", owner.as_ref()], program_id)
+}
 
 #[test]
 fn test_initialize() {
@@ -16,10 +25,19 @@ fn test_initialize() {
     svm.add_program(program_id, bytes).unwrap();
     svm.airdrop(&payer.pubkey(), 1_000_000_000).unwrap();
 
+    let (vault_state, _) = get_vault_state_pda(&payer.pubkey(), &program_id);
+    let (vault, _) = get_vault_pda(&payer.pubkey(), &program_id);
+
     let instruction = Instruction::new_with_bytes(
         program_id,
         &anchor_vault::instruction::Initialize {}.data(),
-        anchor_vault::accounts::Initialize {}.to_account_metas(None),
+        anchor_vault::accounts::Initialize {
+            owner: payer.pubkey(),
+            vault_state,
+            vault,
+            system_program: anchor_lang::solana_program::system_program::ID,
+        }
+        .to_account_metas(None),
     );
 
     let blockhash = svm.latest_blockhash();
