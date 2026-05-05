0d00bc07dcd8efa35ff8e21f28b6334d07d6aa2c
drago <93286752+drago44@users.noreply.github.com>
2026-05-05 18:30:21 +0300

    feat: add staking error codes

--- 07-staking-protocol/programs/staking_protocol/src/error.rs
+++ 07-staking-protocol/programs/staking_protocol/src/error.rs
@@ -1,7 +1,17 @@
 use anchor_lang::prelude::*;
 
 #[error_code]
-pub enum ErrorCode {
-    #[msg("Custom error message")]
-    CustomError,
+pub enum StakingError {
+    #[msg("Amount must be greater than zero")]
+    ZeroAmount,
+    #[msg("Insufficient staked amount")]
+    InsufficientStake,
+    #[msg("Arithmetic overflow")]
+    MathOverflow,
+    #[msg("Cooldown period has not expired")]
+    CooldownNotExpired,
+    #[msg("Unauthorized")]
+    Unauthorized,
+    #[msg("Invalid mint")]
+    InvalidMint,
 }
